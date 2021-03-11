f4bb711da395f5c31496c6f16789abff1bfa603c
eiparfenov <epar2002@outlook.com>
2021-03-11 13:01:31 +0300

    add Controls script

---
+++ Assets/Aircraft/Scripts/Controls/Controls.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    public float Yaw { get; private set; } = 0f; // –ыскание
+    public float Roll { get; private set; } = 0f; //  рен
+    public float Pitch { get; private set; } = 0f; // “ангаж
+    public float Pull { get; private set; } = 0f; // “€га
+    public bool GearsOut { get; private set; } = true;// Ўасси
+    public bool Breaks { get; private set; } = false;// “ормоза
+
+    private void Update()
+    {
+        // ”правление рысканием на клавиши Q и E
+        Yaw = 0; 
+        if (Input.GetKey(KeyCode.Q))
+            Yaw += 1;
+        if (Input.GetKey(KeyCode.E))
+            Yaw -= 1;
+
+        // ”правление тангажем на клавиши W и S
+        Pitch = Input.GetAxis("Horizontal");
+
+        // ”правление креном на клавиши A и D
+        Roll = Input.GetAxis("Vertical");
+
+        // ”правление т€гой на LeftShift и LeftCtrl
+        if (Input.GetKey(KeyCode.LeftShift)) 
+            Pull = Mathf.Clamp(Pull + Time.deltaTime, 0f, 1f);
+        if (Input.GetKeyDown(KeyCode.LeftCommand))
+            Pull = Mathf.Clamp(Pull - Time.deltaTime, 0f, 1f);
+
+        // ”борка и выпуск шасси на клавише G
+        if (Input.GetKeyDown(KeyCode.G))
+            GearsOut = !GearsOut;
+
+        // “орможение включаетс€ при нулевой т€ге и зажатой клавише уменьшени€ т€ги LeftCtrl
+        if (Input.GetKey(KeyCode.LeftCommand) && Pull == 0f)
+            Breaks = true;
+        else
+            Breaks = false;
+    }
+}
