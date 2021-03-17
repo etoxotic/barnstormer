ede3292141f752b0c940fe932733fc5b5e83d0e8
Yana <etoxotic@gmail.com>
2021-03-17 16:22:14 +0300

    do tutorial map + flat center terrain

---
+++ Assets/Scripts/HeightMapGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeightMapGenerator
+{
+    public static HeightMap GenerateHeightMap(int width, int height, HeightMapSettings settings, EdgesData edgesData, Vector2 sampleCenter)
+    {
+        float[,] values = Noise.GenerateNoiseMap(width, height, settings.noiseSettings, sampleCenter);
+        float[,] edgeMap = EdgeMapGenerator.GenerateEdgeMap(edgesData, width);
+
+        AnimationCurve heightCurve_threadsafe = new AnimationCurve(settings.heightCurve.keys);
+
+        float minValue = float.MaxValue;
+        float maxValue = float.MinValue;
+
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                float value = Mathf.Clamp01(values[i, j] - edgeMap[i, j]);
+                values[i, j] *= heightCurve_threadsafe.Evaluate(value) * settings.heightMultiplier;
+
+                if (values[i, j] > maxValue)
+                {
+                    maxValue = values[i, j];
+                }
+
+                if (values[i, j] < minValue)
+                {
+                    minValue = values[i, j];
+                }
+            }
+        }
+
+        return new HeightMap(values, minValue, maxValue);
+    }
+}
+
+public struct HeightMap
+{
+    public readonly float[,] values;
+    public readonly float minValue;
+    public readonly float maxValue;
+
+    public HeightMap(float[,] values, float minValue, float maxValue)
+    {
+        this.values = values;
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+
+    }
+}
